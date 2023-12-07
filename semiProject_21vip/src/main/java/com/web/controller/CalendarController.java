e50506631e02ab0628ae6f6a70bf9314b24d4c78
cromakicookie <hey9908gg@gmail.com>
2023-12-07 13:32:58 +0900

    main ~ Calendar

--- semiProject_21vip/src/main/java/com/web/controller/CalendarController.java
+++ semiProject_21vip/src/main/java/com/web/controller/CalendarController.java
@@ -1,14 +1,19 @@
 package com.web.controller;
 
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.web.domain.Calendar;
+import com.web.domain.Member;
 import com.web.persistence.CalendarRepository;
 import com.web.service.CalendarService;
+import com.web.service.MainService;
 
 
 @Controller
@@ -27,5 +32,11 @@ public class CalendarController {
 		cs.insertCalendar(cal);
 		return "redirect:calendar";
 	}
-
+	
+	@GetMapping("mypage")
+    public String myPage(Model model, @ModelAttribute("user") Member member) {
+//		Member findMember = MainService.getMember(member);
+//        model.addAttribute("member",findMember);
+        return "calendar/mypage";
+    }
 }
