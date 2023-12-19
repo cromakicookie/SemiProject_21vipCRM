8de3d9b0bca271c57330fa183f580b6eb9188b2b
dldbs <dldbs@Mingi2275>
2023-12-19 09:20:13 +0900

    Merge branch 'develop' of https://github.com/appletella/SemiProject_21vipCRM.git into develop

--- semiProject_21vip/src/main/java/com/web/service/LuxuryService.java
+++ semiProject_21vip/src/main/java/com/web/service/LuxuryService.java
@@ -1,16 +1,20 @@
 package com.web.service;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.web.domain.Customer;
 import com.web.domain.Luxury;
 
 public interface LuxuryService {
 	
 	void insertLuxury(Luxury luxury);
 	
-//	Page<Luxury> findCustomerNum(String searchKeyword, Pageable pageable);
+	Page<Luxury> findCustomerNum(String searchKeyword, Pageable pageable);
 	
 	Page<Luxury> PageList(Pageable pageable);
 	
@@ -22,9 +26,10 @@ public interface LuxuryService {
 	
 	Page<Luxury> findBrand(String brand, Pageable pageable);
 	
-//	Page<Luxury> findCustomerNumBrand(String searchKeyword, Pageable pageable, String brand);
+	Page<Luxury> findCustomerNumBrand(String searchKeyword, Pageable pageable, String brand);
 	
+	Customer getCustomer(String customerNum);
 	
+	public Map<String, Object> searchCustomer(String customerNum);
 	
-//	public Customer searchCustomer(String num); 
 }
